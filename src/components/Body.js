b8f9cacf9068218da45a2e16c474e322177ce4dc
Adil Iqbal <adiliqbal@adils-mbp.home>
2025-01-18 20:32:44 -0500

    added search restaurant feature

--- src/components/Body.js
+++ src/components/Body.js
@@ -6,6 +6,11 @@ const Body = () => {
 
     const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
+    const [filteredRestaurants,setFilteredRestaurants] = useState([]);
+
+
+    const [searchText,setSearchText] = useState("");
+
     useEffect(() => {
         fetchData();
     }, []);
@@ -15,6 +20,7 @@ const Body = () => {
         const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5142176&lng=73.7774778&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
         const json = await data.json();
         setListOfRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+        setFilteredRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     }
 
 
@@ -22,16 +28,24 @@ const Body = () => {
     
         <div className="filter-button">
 
+            <div className="search-container">
+                <input value={searchText} onChange={(e) => setSearchText(e.target.value)}/>
+                <button onClick={() => {
+                    let filteredRes = listOfRestaurants.filter(restaurant => restaurant.info.name.toLowerCase().includes(searchText.toLowerCase()));
+                    setFilteredRestaurants(filteredRes);
+                }}>Search</button>
+            </div>
+
             <button onClick={() => {
-                let filteredList = listOfRestaurants.filter(res => res.rating > 4.5);
-                setListOfRestaurants(filteredList);
+                let filteredList = listOfRestaurants.filter(res => res.info.avgRating > 4.5);
+                setFilteredRestaurants(filteredList);
             }}>Top Rated Restaurants</button>  
 
         </div>
 
 
         <div className="restaurants-container">
-            {listOfRestaurants.map(restaurant => (
+            {filteredRestaurants.map(restaurant => (
                 <ResCard key={restaurant.info.id} restaurant={restaurant.info}/>
             ))}
         </div>
